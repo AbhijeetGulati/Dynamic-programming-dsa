9adec0d8213f390085619ed701705e44de11b559
AbhijeetGulati <115371937+AbhijeetGulati@users.noreply.github.com>
2022-12-21 17:57:26 +0530

    Update dp.java

--- dp.java
+++ dp.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 
 public class dp {
+   /*q1 You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+*/
    
 
     //main function
@@ -11,6 +17,8 @@ public class dp {
         System.out.println(ans);
 
     }//end of void main function
+   //way 1
+   //TABULATION
     public static int minCostClimbingStairs(int[] cost) {
         //now here we will find our final ans
             int n=cost.length;
@@ -25,4 +33,31 @@ public class dp {
             }
             return dp[n-1];
         }
+   
+   
+   //way 2 using memoization
+    public static int minCostClimbingStairs(int[] cost) {
+    //now here we will find our final ans
+    //MEMOIZATION
+        int n=cost.length;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        int mini=Integer.MAX_VALUE;
+        return Math.min(f(n-1,cost,dp),f(n-2,cost,dp));
+    }
+    public static int f(int index,int[] cost,int[] dp){
+        //base case
+        if(index==0 || index==1){
+            return cost[index];
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        //recurrence
+        int single=f(index-1,cost,dp)+cost[index];
+        int two=f(index-2,cost,dp)+cost[index];
+        return dp[index]=Math.min(single,two);
+
+
+    }
 }//end of dp class
